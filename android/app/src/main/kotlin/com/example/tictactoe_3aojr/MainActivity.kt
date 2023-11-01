ab1681fe1439deb129b535e8dc2f04fd47ed427d
Alan Franzin <rafael.thomazelli@gmail.com>
2023-10-31 21:19:29 -0300

    Aula 05 parte 01

--- android/app/src/main/kotlin/com/example/tictactoe_3aojr/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tictactoe_3aojr/MainActivity.kt
@@ -1,6 +1,101 @@
 package com.example.tictactoe_3aojr
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import android.util.Log
+import com.google.gson.JsonElement
+import com.pubnub.api.PNConfiguration
+import com.pubnub.api.PubNub
+import com.pubnub.api.callbacks.SubscribeCallback
+import com.pubnub.api.models.consumer.PNStatus
+import com.pubnub.api.models.consumer.objects_api.channel.PNChannelMetadataResult
+import com.pubnub.api.models.consumer.objects_api.membership.PNMembershipResult
+import com.pubnub.api.models.consumer.objects_api.uuid.PNUUIDMetadataResult
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult
+import com.pubnub.api.models.consumer.pubsub.PNSignalResult
+import com.pubnub.api.models.consumer.pubsub.files.PNFileEventResult
+import com.pubnub.api.models.consumer.pubsub.message_actions.PNMessageActionResult
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.util.Arrays
 
 class MainActivity: FlutterActivity() {
+
+    private val METHOD_CHANNEL = "game/exchange"
+
+    private var channel_pubNub: String? = null
+    private var pubNub: PubNub? = null
+    private var handler: Handler? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        handler = Handler(Looper.getMainLooper())
+
+        val pnConfiguration = PNConfiguration("MyUniqueUUID")
+        pnConfiguration.subscribeKey = "sub-c-c84cf3ae-c7da-49d5-a044-c3b55b84bed9"
+        pnConfiguration.publishKey = "pub-c-8290cf60-30c1-4e75-bdb7-b3ac46a0282b"
+
+        pubNub = PubNub(pnConfiguration)
+
+        pubNub.let {
+            it?.addListener( object : SubscribeCallback(){
+                override fun status(pubnub: PubNub, pnStatus: PNStatus) {}
+                override fun presence(pubnub: PubNub, pnPresenceEventResult: PNPresenceEventResult) {}
+                override fun signal(pubnub: PubNub, pnSignalResult: PNSignalResult) {}
+                override fun uuid(pubnub: PubNub, pnUUIDMetadataResult: PNUUIDMetadataResult) {}
+                override fun channel(pubnub: PubNub, pnChannelMetadataResult: PNChannelMetadataResult) {}
+                override fun membership(pubnub: PubNub, pnMembershipResult: PNMembershipResult) {}
+                override fun messageAction(pubnub: PubNub, pnMessageActionResult: PNMessageActionResult) {}
+                override fun file(pubnub: PubNub, pnFileEventResult: PNFileEventResult) {}
+
+                override fun message(pubnub: PubNub, pnMessageResult: PNMessageResult) {
+                    Log.d("Pubnub Listener", "Received Mesage: R{pnMessageResult.message.toString()}")
+                    var receivedObject: JsonElement? = null
+                    var actionReceived = "sendAction"
+
+                    if(pnMessageResult.message.asJsonObject["tap"] !== null){
+                        receivedObject = pnMessageResult.message.asJsonObject["tap"]
+                    }
+
+                    handler?.let {
+                        it.post {
+                            val methodChannel = MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, METHOD_CHANNEL)
+                            methodChannel.invokeMethod(actionReceived, receivedObject.toString())
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        val methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, METHOD_CHANNEL)
+        methodChannel.setMethodCallHandler { call, result ->
+            if(call.method == "sendAction"){
+                pubNub!!.publish()
+                        .message(call.arguments)
+                        .channel(channel_pubNub)
+                        .async { _, status -> Log.d("pubnub", "teve erro? ${status.isError}")}
+                result.success(true)
+            }
+            else if(call.method == "subscribe"){
+                subscribeChannel(call.argument<String>("channel"))
+                result.success(true)
+            }
+        }
+    }
+
+    fun subscribeChannel(channelName: String?){
+        channel_pubNub = channelName
+        channelName.let {
+            pubNub?.subscribe()?.channels(Arrays.asList(channelName))?.execute()
+        }
+    }
 }
--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tictactoe_3aojr/models/game.dart';
 import 'package:tictactoe_3aojr/utils/constants.dart';
@@ -11,7 +12,8 @@ class GameWidget extends StatefulWidget {
 }
 
 class _GameWidgetState extends State<GameWidget> {
-  
+  static const platform = MethodChannel("game/exchange");
+
   Game? game;
   bool suaVez = false;
   List<List<int>> cells = [
@@ -19,6 +21,43 @@ class _GameWidgetState extends State<GameWidget> {
     [0, 0, 0],
     [0, 0, 0]
   ];
+
+  final dialogTitle = const Text("Qual é o nome do jogo?");
+  final buttonPlay = const Text("Jogar");
+  final buttonCancel = const Text("Cancelar");
+
+  @override
+  void initState() {
+    super.initState();
+    _configureMethodChannelCallback();
+  }
+
+  _configureMethodChannelCallback() {
+    platform.setMethodCallHandler((call) async {
+      print('Flutter Platform received $call');
+
+      final action = call.method;
+      final arguments = call.arguments.toString().replaceAll("\"", "");
+      final splitted = arguments.split("|");
+
+      if (action == "sendAction") {
+        final player = splitted[0];
+        final x = int.parse(splitted[1]);
+        final y = int.parse(splitted[2]);
+
+        print('player $player, x $x e y $y');
+
+        if (player == (game!.creator ? "p2" : "p1")) {
+          setState(() {
+            suaVez = true;
+            cells[x][y] = 2;
+          });
+
+          _checkWinner();
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,17 +96,17 @@ class _GameWidgetState extends State<GameWidget> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        (game == null ? 
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              _buildButton("Criar", true),
-                              const SizedBox(width: 10),
-                              _buildButton("Entrar", false),
-                            ],
-                          )
-                          : Text( suaVez ? "Sua vez!" : "Aguarde sua vez", style: textStyle36)),
-                        
+                        (game == null
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  _buildButton("Criar", true),
+                                  const SizedBox(width: 10),
+                                  _buildButton("Entrar", false),
+                                ],
+                              )
+                            : Text(suaVez ? "Sua vez!" : "Aguarde sua vez",
+                                style: textStyle36)),
                         GridView.count(
                             shrinkWrap: true,
                             mainAxisSpacing: 10,
@@ -90,16 +129,17 @@ class _GameWidgetState extends State<GameWidget> {
     );
   }
 
-  Widget _buildButton(String label, bool isCreator){
+  Widget _buildButton(String label, bool isCreator) {
     return SizedBox(
-      width: ScreenUtil().setWidth(300),
-      child: OutlinedButton(
-        style: OutlinedButton.styleFrom(side: borderDefault),
-        child: Padding(padding: paddingDefault,child: Text(label, style: textStyle36)),
-        onPressed: () {
-          
-        })
-    );
+        width: ScreenUtil().setWidth(300),
+        child: OutlinedButton(
+            style: OutlinedButton.styleFrom(side: borderDefault),
+            child: Padding(
+                padding: paddingDefault,
+                child: Text(label, style: textStyle36)),
+            onPressed: () {
+              _createGame(isCreator);
+            }));
   }
 
   Widget _getCell(int x, int y) {
@@ -117,6 +157,187 @@ class _GameWidgetState extends State<GameWidget> {
                 style: textStyle72),
           ),
         ),
-        onTap: () {});
+        onTap: () async {
+          if (suaVez == true && cells[x][y] == 0) {
+            final result = await _sendAction(
+                "sendAction", {"tap": "${game!.creator ? "p1" : "p2"}|$x|$y"});
+            if (result) {
+              setState(() {
+                suaVez = false;
+                cells[x][y] = 1;
+              });
+            }
+
+            _checkWinner();
+          }
+        });
+  }
+
+  Future _createGame(bool isCreator) {
+    final editingController = TextEditingController();
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: dialogTitle,
+            content: TextField(controller: editingController),
+            actions: [
+              ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    final result = await _sendAction(
+                        "subscribe", {"channel": editingController.text});
+                    if (result) {
+                      setState(() {
+                        game = Game(editingController.text, isCreator);
+                        suaVez = isCreator;
+                      });
+                    }
+                  },
+                  child: buttonPlay),
+              ElevatedButton(
+                  onPressed: () => Navigator.pop(context), child: buttonCancel),
+            ],
+          );
+        });
+  }
+
+  Future<bool> _sendAction(
+      String action, Map<String, dynamic> arguments) async {
+    try {
+      final result = await platform.invokeMethod(action, arguments);
+      if (result) {
+        return true;
+      }
+    } on PlatformException catch (e) {
+      print('Ocorreu erro ao enviar para plataforma nativa: $e');
+    }
+
+    return false;
+  }
+
+  _checkWinner() {
+    bool youWin = false;
+    bool otherWin = false;
+    if (cells[0][0] != 0 &&
+        cells[0][0] == cells[0][1] &&
+        cells[0][0] == cells[0][2]) {
+      if (cells[0][0] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[1][0] != 0 &&
+        cells[1][0] == cells[1][1] &&
+        cells[1][0] == cells[1][2]) {
+      if (cells[1][0] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[2][0] != 0 &&
+        cells[2][0] == cells[2][1] &&
+        cells[2][0] == cells[2][2]) {
+      if (cells[2][0] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[0][0] != 0 &&
+        cells[0][0] == cells[1][0] &&
+        cells[0][0] == cells[2][0]) {
+      if (cells[0][0] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[0][1] != 0 &&
+        cells[0][1] == cells[1][1] &&
+        cells[0][1] == cells[2][1]) {
+      if (cells[0][1] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[0][2] != 0 &&
+        cells[0][2] == cells[1][2] &&
+        cells[0][2] == cells[2][2]) {
+      if (cells[0][2] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[0][0] != 0 &&
+        cells[0][0] == cells[1][1] &&
+        cells[0][0] == cells[2][2]) {
+      if (cells[0][0] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    } else if (cells[0][2] != 0 &&
+        cells[0][2] == cells[1][1] &&
+        cells[0][2] == cells[2][0]) {
+      if (cells[0][2] == 1) {
+        youWin = true;
+      } else {
+        otherWin = true;
+      }
+    }
+
+    if (youWin) {
+      _showFinishGame(true);
+    } else if (otherWin) {
+      _showFinishGame(false);
+    } else {
+      bool allPlaysDone = true;
+
+      for (var line in cells) {
+        for (var col in line) {
+          if (allPlaysDone == false) break;
+          if (col == 0) {
+            allPlaysDone = false;
+            break;
+          }
+        }
+      }
+
+      if (allPlaysDone) {
+        _showFinishGame(null);
+      }
+    }
+  }
+
+  Future _showFinishGame(bool? youWin) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("FIM DE JOGO"),
+            content: Text(youWin == true
+                ? "Você ganhou"
+                : youWin == false
+                    ? "Você perdeu"
+                    : "Não houve Ganhadores"),
+            actions: [
+              ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    setState(() {
+                      game = null;
+                      suaVez = false;
+                      cells = [
+                        [0, 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]
+                      ];
+                    });
+                  },
+                  child: const Text("Fechar")),
+            ],
+          );
+        });
   }
 }
